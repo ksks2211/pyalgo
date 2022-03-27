4674d480e9eb180593705a5ff6113c519fd905fc
ksks2211 <rival15@naver.com>
2022-03-27 14:13:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PyAlgo
+
+Algorithm note
+
+## Installation
+
+```bash
+pip install git+https://github.com/ksks2211/pyalgo.git
+```
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="pyalgo",
+    version="0.0.1",
+    author="pyy",
+    author_email="rival15@naver.com",
+    description="personal algorithm note",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkll22/pyalgo",
+    project_urls={"Bug Tracker": "https://github.com/kkll22/pyalgo/issues"},
+    license="MIT",
+    packages=["sorting"],
+    install_requires=[],
+)
