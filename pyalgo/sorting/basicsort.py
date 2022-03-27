faa6dd3ec054e2ba2dc415cd5a8e3a234bf5d170
ksks2211 <rival15@naver.com>
2022-03-27 14:44:04 +0900

    add inplace option

--- pyalgo/sorting/basicsort.py
+++ pyalgo/sorting/basicsort.py
@@ -1,6 +1,12 @@
-# Bubble Sort
-def bubble_sort(arr):
 
+def get_arr(arr,inplace):
+    return arr if inplace is True else arr[:]
+
+
+# Bubble Sort
+def bubble_sort(arr,**kwargs):
+    arr = get_arr(arr,kwargs.get('inplace'))
+    
     #  len(arr)-1   >= (i) >= 1
     for i in range(len(arr) - 1, 0, -1):
         swap = False
@@ -11,10 +17,13 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
         if not swap:
             break
+    return arr            
 
 
 # Selection Sort
 def selection_sort(arr):
+    arr = get_arr(arr,kwargs.get('inplace'))
+
     #  0 <= (i) <= len(arr)-2
     for i in range(len(arr) - 1):
         sm_idx = i
@@ -24,10 +33,13 @@ def selection_sort(arr):
                 sm_idx = j
         # swap
         arr[sm_idx], arr[i] = arr[i], arr[sm_idx]
+    return arr        
 
 
 # Insertion Sort
 def insertion_sort(arr):
+    arr = get_arr(arr,kwargs.get('inplace'))
+
     for i in range(1, len(arr)):
         # 추가될 값
         target = arr[i]
@@ -38,3 +50,21 @@ def insertion_sort(arr):
             arr[k] = arr[k - 1]
             k -= 1
         arr[k] = target
+    return arr        
+
+
+
+if __name__=="__main__":
+
+    list = [1,5,2,6,4]
+    sorted = bubble_sort(list)
+
+    print(list)
+    print(sorted)
+
+    list = [1,5,2,6,4]
+    sorted = bubble_sort(list,inplace=True)
+
+    print(list)
+    print(sorted)
+
