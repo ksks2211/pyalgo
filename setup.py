49f9b3ff47ea71133502270fbee480f7a7c44043
ksks2211 <rival15@naver.com>
2022-04-10 22:49:44 +0900

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyalgo",
-    version="0.0.1",
+    version="0.0.2",
     author="pyy",
     author_email="rival15@naver.com",
     description="personal algorithm note",
